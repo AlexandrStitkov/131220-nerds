df29e66f26905290d5cda1fa038e6955abc68dc9
AlexandrStitkov <sas@is4business.ru>
2017-05-16 19:05:45 +0300

    fix progect

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var link = document.querySelector(".btn-map");
 var popup = document.querySelector(".modal");
 var close = document.querySelector(".close");
-var form = popup.querySelector("form");
+var form = popup.querySelector(".contact-form");
 var name = form.querySelector("[name=name]");
 var email = form.querySelector("[name=e-mail]");
 
@@ -14,11 +14,46 @@ close.addEventListener("click", function (evt) {
   evt.preventDefault();
   console.log("Закрыть модальное окно");
   popup.classList.remove("modal-show");
+  form.classList.remove('form-invalid');
+  form.classList.remove('modal-error');
 });
 form.addEventListener("submit", function(evt){
   if (!name.value && !email.value) {
     evt.preventDefault();
     form.classList.add('form-invalid');
+    form.classList.add('modal-error');
     console.log("Заполните пустые поля");
+  } else {
+    localStorage.setItem("name", name.value);
   }
+});
+/*Переключение слайдов*/
+var slides1 = document.querySelector("#slide-1");
+var slides2 = document.querySelector("#slide-2");
+var slides3 = document.querySelector("#slide-3");
+/*Навигация слайдов*/
+var slide1 = document.querySelector(".slide-1");
+var slide2 = document.querySelector(".slide-2");
+var slide3 = document.querySelector(".slide-3");
+
+slide1.addEventListener("click", function(est) {
+  est.preventDefault();
+  console.log("вы нажали первый слайд");
+  slides1.classList.remove("hidden");
+  slides2.classList.add("hidden");
+  slides3.classList.add("hidden");
+});
+slide2.addEventListener("click", function(est) {
+  est.preventDefault();
+  console.log("вы нажали второй слайд");
+  slides1.classList.add("hidden");
+  slides2.classList.remove("hidden");
+  slides3.classList.add("hidden");
+});
+slide3.addEventListener("click", function(est) {
+  est.preventDefault();
+  console.log("вы нажали третий слайд");
+  slides1.classList.add("hidden");
+  slides2.classList.add("hidden");
+  slides3.classList.remove("hidden");
 });
